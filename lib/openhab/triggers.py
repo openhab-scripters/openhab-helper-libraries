2ded4111309fdda4f210fcb9fe4a04e1934be2e6
IanH <ian_web1@hubbertz.de>
2017-08-14 22:00:58 +0200

    1st try to add "item_group_triggered" as decorator (DOES NOT WORK YET!)

--- lib/openhab/triggers.py
+++ lib/openhab/triggers.py
@@ -4,6 +4,9 @@ from org.eclipse.smarthome.automation import Trigger
 from org.eclipse.smarthome.automation.handler import TriggerHandler
 from org.eclipse.smarthome.automation.type import TriggerType
 from org.eclipse.smarthome.config.core import Configuration
+#from org.eclipse.smarthome.core.items import ItemRegistry
+from openhab.log import logging
+
 
 import openhab
 from openhab.jsr223 import scope, get_automation_manager
@@ -103,12 +106,17 @@ def item_group_triggered(group_name, event_types=None, result_item_name=None):
     if hasattr(event_types, '__iter__'):
         event_types = ",".join(event_types)
     def decorator(fn):
+        dlog = logging.getLogger("RULES.oh2-jython.triggers.item_group_triggered")
+
         def callback(module, inputs):
             result_value = fn()
             if result_item_name:
                 scope.events.postUpdate(result_item_name, str(result_value))
         group_triggers = []
-        #[ItemEventTrigger(item_name, event_types)]
+        groupitems = [] #itemRegistry.getItem(group_name)
+        for i in groupitems.getAllMembers():
+            group_triggers.append(ItemEventTrigger(unicode(i.name)), event_types)
+            dlog.debug("   added ItemStateUpdateTrigger for " + unicode(i.name))
         rule = _FunctionRule(callback, group_triggers, extended=True)
         get_automation_manager().addRule(rule)
         return fn
