66ab8bf7f295856f337737664f3b68de49b57ca8
Ralph Borchers <ralle12345@users.noreply.github.com>
2017-10-22 20:11:16 +0200

    ChannelEventTrigger added

---
+++ scripts/examples/channel_event_example.py
@@ -0,0 +1,14 @@
+from openhab.triggers import ChannelEventTrigger
+from openhab.rules import rule, addRule
+
+@rule
+class channelEventExample(object):
+
+    def get_event_triggers(self):__init__(self):
+        return [ChannelEventTrigger('astro:sun:away:set#event','START','Sunset_Away') ])
+                    
+    def execute(self,modules,inputs):
+        self.log.info('Sunset triggered')
+
+addRule(channelEventExample())
+      
