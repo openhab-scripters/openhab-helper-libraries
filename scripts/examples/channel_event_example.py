9fbbe5025d5c9c5c3e032e532096ff2cca34a082
Steve Bate <steve-bate@users.noreply.github.com>
2018-01-12 06:06:06 -0500

    Removed extraneous __init__

--- scripts/examples/channel_event_example.py
+++ scripts/examples/channel_event_example.py
@@ -4,7 +4,7 @@ from openhab.rules import rule, addRule
 @rule
 class channelEventExample(object):
 
-    def get_event_triggers(self):__init__(self):
+    def get_event_triggers(self):
         return [ChannelEventTrigger('astro:sun:away:set#event','START','Sunset_Away') ])
                     
     def execute(self,modules,inputs):
