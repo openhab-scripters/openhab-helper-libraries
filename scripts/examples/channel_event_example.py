0b7ddacc3798453ff8b61642d2d954be58a7d51e
Steve Bate <steve-bate@users.noreply.github.com>
2018-01-12 06:07:56 -0500

    Removed extra paren

--- scripts/examples/channel_event_example.py
+++ scripts/examples/channel_event_example.py
@@ -5,7 +5,7 @@ from openhab.rules import rule, addRule
 class channelEventExample(object):
 
     def get_event_triggers(self):
-        return [ChannelEventTrigger('astro:sun:away:set#event','START','Sunset_Away') ])
+        return [ChannelEventTrigger('astro:sun:away:set#event','START','Sunset_Away')]
                     
     def execute(self,modules,inputs):
         self.log.info('Sunset triggered')
