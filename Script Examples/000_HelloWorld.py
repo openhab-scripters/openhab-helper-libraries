ccdcc141aa1501027b2595c9199a2e39d0805e24
Scott Rushworth <21967853+openhab-5iver@users.noreply.github.com>
2018-10-06 14:23:46 -0400

    Moved rule decorator to openhab.rules, moved triggers list to a function attribute, and updated readme (#23)
    * Changed directory structure and updated README.md
    
    * Removed vscode setting file
    
    * Fixed bookmark in README.md
    
    * Fixed some bookmarks in README.md
    
    * More bookmark fixes to README.md
    
    * Major update to triggers.py and small fix for rules.rule
    
    * Updated README.md to include updated rule decorator
    
    * Moved rule decorator to rules.py and updated readme

---
+++ Script Examples/000_HelloWorld.py
@@ -0,0 +1,60 @@
+from org.slf4j import Logger, LoggerFactory
+from openhab.rules import rule, CronTrigger
+from openhab.triggers import when
+scriptExtension.importPreset("RuleSupport")
+scriptExtension.importPreset("RuleSimple")
+
+log = LoggerFactory.getLogger("org.eclipse.smarthome.model.script.Rules")
+'''
+class RawAPICron(SimpleRule):
+    def __init__(self):
+        self.triggers = [
+            TriggerBuilder.create()
+                    .withId("Hello_World_Timer_Trigger")
+                    .withTypeUID("timer.GenericCronTrigger")
+                    .withConfiguration(
+                        Configuration({
+                            "cronExpression": "0/10 * * * * ?"
+                        })).build()
+        ]
+
+    def execute(self, module, inputs):
+        log.debug("JSR223: This is a 'hello world!' from a Jython rule (raw API): Cron")
+
+automationManager.addRule(RawAPICron())
+
+# make sure you have an item named Test_Switch_1, or swap out the item for one that exists
+class RawAPIStateUpdate(SimpleRule):
+    def __init__(self):
+        self.triggers = [
+            TriggerBuilder.create()
+                .withId("Hello_World_State_Update_Trigger")
+                .withTypeUID("core.ItemStateUpdateTrigger")
+                .withConfiguration(
+                    Configuration({
+                        "itemName": "Test_Switch_1"
+                    }))
+                .build()
+        ]
+        log.debug("JSR223: self.triggers=[{}]".format(self.triggers))
+
+    def execute(self, module, input):
+        log.debug("JSR223: This is a 'hello world!' from a Jython rule (raw API): ItemStateUpdateTrigger")
+
+automationManager.addRule(RawAPIStateUpdate())
+
+# requires CronTrigger import
+class ExtensionCron(SimpleRule):
+    def __init__(self):
+        self.triggers = [ CronTrigger("0/10 * * * * ?") ]
+    
+    def execute(self, module, inputs):
+        log.debug("JSR223: This is a 'hello world!' from a Jython rule (extension): Cron")
+
+automationManager.addRule(ExtensionCron())
+'''
+# requires rule and when imports
+@rule("Hello World timer rule")
+@when("Time cron 0/10 * * * * ?")
+def hellowWorldDecorator(event):
+    log.debug("JSR223: This is a 'hello world!' from a Jython rule (decorator): Cron")
