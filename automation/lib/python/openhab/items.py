7a29e60dd8cd85ce279566bade0e1c72bbbb594c
Scott Karns <scott.karns@videastream.com>
2018-10-31 07:21:03 -0600

    Reworked items.add() to use builder style item creation

--- automation/lib/python/openhab/items.py
+++ automation/lib/python/openhab/items.py
@@ -1,19 +1,37 @@
 # NOTE: Requires JythonItemProvider component
 
+from org.slf4j import Logger, LoggerFactory
+
 from openhab import osgi, jsr223, JythonItemProvider
-from org.eclipse.smarthome.core.library.items import StringItem
-from org.eclipse.smarthome.core.items import Item, ItemProvider
+from openhab.jsr223 import scope
 
 __all__ = ["add", "remove"]
 
-item_factory = osgi.get_service("org.eclipse.smarthome.core.items.ItemFactory")
 
-def add(item, item_type=None):
-    if isinstance(item, str):
-        if item_type is None:
-            raise Exception("Must provide item_type when creating an item by name")
-        item = item_factory.createItem(item_type, item)
-    JythonItemProvider.add(item)
+logger = LoggerFactory.getLogger("org.eclipse.smarthome.model.script.Rules.jsr232.openhab")
+
+def add(item, item_type=None, category=None, groups=None, label=None, gi_base_type=None, group_function=None):
+    try:
+        if isinstance(item, str):
+            if item_type is None:
+                raise Exception("Must provide item_type when creating an item by name")
+
+            baseItem = None if item_type != "Group" or gi_base_type is None else scope.itemRegistry.newItemBuilder( gi_base_type       \
+                                                                                                                  , item + "_baseItem")\
+                                                                                                   .build()
+            group_function = None if item_type != "Group" else group_function
+            item = scope.itemRegistry.newItemBuilder(item_type, item)  \
+                                     .withCategory(category)           \
+                                     .withGroups(groups)               \
+                                     .withLabel(label)                 \
+                                     .withBaseItem(baseItem)           \
+                                     .withGroupFunction(group_function)\
+                                     .build()
+
+        JythonItemProvider.add(item)
+    except:
+        import traceback
+        logger.error(traceback.format_exc())
 
 def remove(item):
     JythonItemProvider.remove(item)
