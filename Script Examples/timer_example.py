a3748e22fc26c0091515e78d580bc9b1a8384e27
Scott Rushworth <openhab@5iver.com>
2018-12-31 12:42:47 -0500

    Corrected incorrected variable name in timer example

--- Script Examples/timer_example.py
+++ Script Examples/timer_example.py
@@ -14,13 +14,13 @@ RUNNABLE - defined
 TIMED_WAITING - timer is running (slight delay after timer is stopped)
 TERMINATED - timer completed or stopped
 '''
-@rule("Example 1 timer rule)
+@rule("Example rule using python threading.timer")
 @when("Item Outlet9_Power changed")
 def batteryChargingMonitor1(event):
     #log.debug("Battery charging monitor: {}: start".format(event.itemState))
     global chargerTimer1
     if items["Outlet9"] == OnOffType.ON and event.itemState <= DecimalType(8) and event.oldItemState <= DecimalType(8):
-        if chargerTimer1 is None or str(chargerTimerAttached.getState()) == "TERMINATED":
+        if chargerTimer1 is None or str(chargerTimer1.getState()) == "TERMINATED":
             chargerTimer1 = Timer(600, lambda: events.sendCommand("Outlet9","OFF"))
             chargerTimer1.start()
             log.info("Battery charging monitor: Started battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
@@ -34,7 +34,7 @@ from org.eclipse.smarthome.model.script.actions.ScriptExecution import createTim
 from org.joda.time import DateTime
 chargerTimer2 = None
 
-@rule("Example 2 timer rule)
+@rule("Example rule using createTimer")
 @when("Item Outlet9_Power changed")
 def batteryChargingMonitor2(event):
     #log.debug("Battery charging monitor: {}: start".format(event.itemState))
