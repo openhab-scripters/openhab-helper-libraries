6fb78f99b1c6f7062b4c6814094648596c2d12e2
Scott Rushworth <openhab@5iver.com>
2019-01-04 04:03:12 -0500

    Updated log entries to use function log attribute

--- Script Examples/timer_example.py
+++ Script Examples/timer_example.py
@@ -1,7 +1,5 @@
 from core.rules import rule
 from core.triggers import when
-from core.log import logging, LOG_PREFIX
-log = logging.getLogger(LOG_PREFIX + ".timer_example")
 
 # Example timer rule using Jython
 from threading import Timer
@@ -23,10 +21,10 @@ def batteryChargingMonitor1(event):
         if chargerTimer1 is None or str(chargerTimer1.getState()) == "TERMINATED":
             chargerTimer1 = Timer(600, lambda: events.sendCommand("Outlet9","OFF"))
             chargerTimer1.start()
-            log.info("Battery charging monitor: Started battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
+            batteryChargingMonitor1.log.info("Battery charging monitor: Started battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
     elif chargerTimer1 is not None and str(chargerTimer1.getState()) == "TIMED_WAITING":
         chargerTimer1.stop()
-        log.info("Battery charging monitor: Cancelled battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
+        batteryChargingMonitor1.log.info("Battery charging monitor: Cancelled battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
 
 # Example using the createTimer Action
 from org.eclipse.smarthome.model.script.actions.Timer import *
@@ -42,7 +40,7 @@ def batteryChargingMonitor2(event):
     if items["Outlet9"] == OnOffType.ON and event.itemState <= DecimalType(8) and event.oldItemState <= DecimalType(8):
         if chargerTimer2 is None or chargerTimer2.hasTerminated():
             chargerTimer2 = creatTimer(DateTime.now().plusMinutes(5), lambda: events.sendCommand("Outlet9","OFF"))
-            log.info("Battery charging monitor: Started battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
+            batteryChargingMonitor2.log.info("Battery charging monitor: Started battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
     elif chargerTimer2 is not None and not chargerTimer2.hasTerminated():
         chargerTimer2.cancel()
-        log.info("Battery charging monitor: Cancelled battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
+        batteryChargingMonitor2.log.info("Battery charging monitor: Cancelled battery charging turn off timer: Outlet9_Power=[{}], oldItemState=[{}]".format(event.itemState,event.oldItemState))
