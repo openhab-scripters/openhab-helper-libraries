55d4bda3ef68f681aebdb640406c61e2a6945d17
Scott Rushworth <openhab@5iver.com>
2019-05-11 08:53:44 -0400

    Fixed some broken examples in hellow_world.py
    Fixes #96
    
    Signed-off-by: Scott Rushworth openhab@5iver.com

--- Script Examples/hello_world.py
+++ Script Examples/hello_world.py
@@ -1,37 +1,122 @@
 from core.rules import rule
-from core.triggers import when, CronTrigger
+from core.triggers import when
 
+@rule("Jython Hello World (cron decorators)", description="This is an example cron triggered rule using decorators", tags=["Test tag", "Hello World"])# [description and tags are optional]
+@when("Time cron 0/10 * * * * ?")
+def hellowWorldCronDecorators(event):
+    hellowWorldCronDecorators.log.info("Hello World!")
+
+# The following cron triggered rules are for demonstration of how the Jython helper libraries have evolved
 '''
+from core.triggers import CronTrigger
+from core.rules import rule
+
+@rule("Jython Hello World (CronTrigger extension with rule decorator)", description="This is an example rule using a CronTrigger extension and rule decorator", tags=["Example rule tag"])
+class CronExtensionWithRuleDecorator(object):
+    def __init__(self):
+        self.triggers = [CronTrigger("0/10 * * * * ?").trigger]
+
+    def execute(self, module, inputs):
+        self.log.info("Hello World!")
+'''
+'''
+from core.triggers import CronTrigger
+from core.log import logging, LOG_PREFIX
+
 scriptExtension.importPreset("RuleSupport")
 scriptExtension.importPreset("RuleSimple")
 
-class RawAPICron(SimpleRule):
+class CronTriggerExtension(SimpleRule):
+    def __init__(self):
+        self.triggers = [CronTrigger("0/10 * * * * ?").trigger]
+        self.name = "Jython Hello World (CronTrigger extension)"
+        self.description = "This is an example Jython rule using a CronTrigger extension"
+        self.tags = set("Example rule tag")
+        self.log = logging.getLogger("{}.Hello World (CronTrigger extension)".format(LOG_PREFIX))
+
+    def execute(self, module, inputs):
+        self.log.info("Hello World!")
+
+automationManager.addRule(CronTriggerExtension())
+'''
+'''
+from org.slf4j import Logger, LoggerFactory
+
+scriptExtension.importPreset("RuleSupport")
+scriptExtension.importPreset("RuleSimple")
+
+class GenericCronTriggerRawAPI(SimpleRule):
     def __init__(self):
         self.triggers = [
             TriggerBuilder.create()
-                    .withId("Hello_World_Cron_Trigger")
+                    .withId("Hello_World_Cron_Trigger")# no spaces akkowed in trigger ID
                     .withTypeUID("timer.GenericCronTrigger")
                     .withConfiguration(
                         Configuration({
                             "cronExpression": "0/10 * * * * ?"
                         })).build()
         ]
-        self.name = "Hello world cron rule (raw API)"
-        self.description = "This is an example cron rule using the raw API"
-        self.tags = [ "Test tag" ]
+        self.name = "Jython Hello World (GenericCronTrigger raw API)"
+        self.description = "This is an example Jython cron rule using the raw API"
+        self.tags = set("Example rule tag")
+        self.log = LoggerFactory.getLogger("jython.Hello World (GenericCronTrigger raw API)")
 
     def execute(self, module, inputs):
-        self.log.info("This is a 'hello world!' from a Jython rule (raw API): Cron")
+        self.log.info("Hello World!")
 
-automationManager.addRule(RawAPICron())
+automationManager.addRule(GenericCronTriggerRawAPI())
 '''
 
+# These rules require an Item names Test_Switch_1
 '''
+from core.rules import rule
+from core.triggers import when
+
+@rule("Jython Hello World (Item update decorators)", description="This is an example Item update triggered rule using decorators", tags=["Example rule tag"])
+@when("Item Test_Switch_1 received update")
+def hellowWorldItemUpdateDecorators(event):
+    hellowWorldItemUpdateDecorators.log.info("Hello World!")
+'''
+# The following Item update triggered rules are for demonstration of how the Jython helper libraries have evolved
+'''
+from core.triggers import ItemStateUpdateTrigger
+from core.rules import rule
+
+@rule("Jython Hello World (ItemStateUpdateTrigger extension with rule decorator)", description="This is an example rule using a ItemStateUpdateTrigger extension and rule decorator", tags=["Example rule tag"])
+class ItemStateUpdateTriggerExtensionWithRuleDecorator(object):
+    def __init__(self):
+        self.triggers = [ItemStateUpdateTrigger("Test_Switch_1").trigger]
+    
+    def execute(self, module, inputs):
+        self.log.info("Hello World!")
+'''
+'''
+from core.triggers import ItemStateUpdateTrigger
+from core.log import logging, LOG_PREFIX
+
 scriptExtension.importPreset("RuleSupport")
 scriptExtension.importPreset("RuleSimple")
 
-# make sure you have an item named Test_Switch_1, or swap out the item for one that already exists
-class RawAPIStateUpdate(SimpleRule):
+class ItemStateUpdateTriggerExtension(SimpleRule):
+    def __init__(self):
+        self.triggers = [ItemStateUpdateTrigger("Test_Switch_1").trigger]
+        self.name = "Jython Hello World (ItemStateUpdateTrigger extension)"
+        self.description = "This is an example rule using an ItemStateUpdateTrigger extension"
+        self.tags = set("Example rule tag")
+        self.log = logging.getLogger("{}.Hello World (ItemStateUpdateTrigger extension)".format(LOG_PREFIX))
+
+    def execute(self, module, inputs):
+        self.log.info("Hello World!")
+
+automationManager.addRule(ItemStateUpdateTriggerExtension())
+'''
+'''
+from org.slf4j import Logger, LoggerFactory
+
+scriptExtension.importPreset("RuleSupport")
+scriptExtension.importPreset("RuleSimple")
+
+class ItemStateUpdateTriggerRawAPI(SimpleRule):
     def __init__(self):
         self.triggers = [
             TriggerBuilder.create()
@@ -43,91 +128,13 @@ class RawAPIStateUpdate(SimpleRule):
                     }))
                 .build()
         ]
-        self.name = "Hello world item state update rule (raw API)"
+        self.name = "Jython Hello World (ItemStateUpdateTrigger raw API)"
+        self.description = "This is an example Jython cron rule using the raw API"
+        self.tags = set("Example rule tag")
+        self.log = logging.getLogger("{}.Hello World (ItemStateUpdateTrigger raw API)".format(LOG_PREFIX))
 
     def execute(self, module, input):
-        self.log.info("This is a 'hello world!' from a Jython rule (raw API): ItemStateUpdateTrigger")
+        self.log.info("Hello World!")
 
-automationManager.addRule(RawAPIStateUpdate())
-'''
-
-'''
-from core.triggers import CronTrigger
-
-scriptExtension.importPreset("RuleSupport")
-scriptExtension.importPreset("RuleSimple")
-
-# requires CronTrigger import
-class ExtensionCron(SimpleRule):
-    def __init__(self):
-        self.triggers = [ CronTrigger("0/10 * * * * ?").trigger ]
-        self.name = "Hello world cron rule (extension)"
-    
-    def execute(self, module, inputs):
-        self.log.info("This is a 'hello world!' from a Jython rule (extension): CronTrigger")
-
-automationManager.addRule(ExtensionCron())
-'''
-
-'''
-from core.triggers import ItemStateUpdateTrigger
-
-scriptExtension.importPreset("RuleSupport")
-scriptExtension.importPreset("RuleSimple")
-
-class ExtensionItemUpdate(SimpleRule):
-    def __init__(self):
-        self.triggers = [ ItemStateUpdateTrigger("Test_Switch_1").trigger ]
-        self.name = "Hello world item state update rule (extension)"
-    
-    def execute(self, module, inputs):
-        self.log.info("This is a 'hello world!' from a Jython rule (extension): ItemStateUpdateTrigger")
-
-automationManager.addRule(ExtensionItemUpdate())
-'''
-
-'''
-from core.triggers import CronTrigger
-from core.rules import rule
-
-@rule("Hello world cron rule (extension with rule decorator)")
-class ExtensionCronWithRule(object):
-    def __init__(self):
-        self.triggers = [ CronTrigger("0/10 * * * * ?").trigger ]
-        self.name = "Hello world cron rule (extension with rule decorator)"
-    
-    def execute(self, module, inputs):
-        self.log.info("This is a 'hello world!' from a Jython rule (extension): CronTrigger")
-'''
-
-'''
-from core.triggers import ItemStateUpdateTrigger
-from core.rules import rule
-
-@rule("Hello world item update rule (extension with rule decorator)")
-class ExtensionItemUpdateWithRule(object):
-    def __init__(self):
-        self.triggers = [ ItemStateUpdateTrigger("Test_Switch_1").trigger ]
-        self.name = "Hello world item update rule (extension with rule decorator)"
-    
-    def execute(self, module, inputs):
-        self.log.info("This is a 'hello world!' from a Jython rule (extension with rule): ItemUpdateTrigger")
-'''
-
-from core.rules import rule
-from core.triggers import when
-
-@rule("Hello World cron rule (decorator)", description="This is an example rule that demonstrates using a cron rule with decorators", tags=["Test tag", "Hello World"])# [description and tags are optional]
-@when("Time cron 0/10 * * * * ?")
-def hellowWorldDecoratorCron(event):
-    hellowWorldDecoratorCron.log.info("This is a 'hello world!' from a Jython rule (decorator): Cron")
-
-'''
-from core.rules import rule
-from core.triggers import when
-
-@rule("Hello World item update rule (decorator)")
-@when("Item Test_Switch_1 received update")
-def hellowWorldDecoratorItemUpdate(event):
-    hellowWorldDecoratorItemUpdate.log.info("This is a 'hello world!' from a Jython rule (decorator): Item update")
+automationManager.addRule(ItemStateUpdateTriggerRawAPI())
 '''
