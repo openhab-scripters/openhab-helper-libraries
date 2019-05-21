fa12f3ad015d8e872a78316ac4596cf163673df0
Scott Rushworth <openhab@5iver.com>
2019-05-20 22:48:29 -0400

    Add other languages (#113)
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

---
+++ Community/Sonos/automation/jsr223/python/community/sonos/example_greeting.py
@@ -0,0 +1,40 @@
+'''
+This script speaks a random greeting every minute on your Sonos speaker system.
+To use this, you should set up astro.py as described here
+https://github.com/OH-Jython-Scripters/openhab2-jython/blob/master/Script%20Examples/Python/Design%20Pattern%20-%20Time%20of%20Day.
+It also assumes that you've set up an openHAB contact items to represent the presence of
+persons to be greeted. Each item should belong to the item group "G_Presence_Family".
+
+Finally make sure that you add the contents of the configuration.py.example file
+into your own configuration file:
+'''
+import random
+
+from core.rules import rule
+from core.triggers import when
+from community.sonos.speak import tts, greeting, PRIO
+from core.utils import getItemValue
+
+@rule("Greeting example")
+@when("Time cron 0 * * * * ?")
+def exampleGreeting(event):
+    greetings = [greeting(), 'Hello', 'How are you', 'How are you doing', 'Good to see you', 'Long time no see', 'Its been a while']
+    peopleAtHome = []
+    for member in itemRegistry.getItem('G_Presence_Family').getAllMembers():
+        if member.state == OPEN: peopleAtHome.append(member.label)
+    random.shuffle(peopleAtHome)
+    msg = random.choice(greetings)
+    for i in range(len(peopleAtHome)):
+        person = peopleAtHome[i]
+        msg += ' '+person
+        if i+2 == len(peopleAtHome):
+            msg +=' and'
+        elif i+1 == len(peopleAtHome):
+            msg +='.'
+        elif i+2 < len(peopleAtHome):
+            msg +=','
+    #tts(msg, PRIO['HIGH'], ttsRoom='Kitchen', ttsVol=42, ttsLang='en-GB', ttsVoice='Brian')
+    tts(msg, PRIO['HIGH'], ttsRoom='Kitchen', ttsVol=42, ttsLang='en-IN', ttsVoice='Aditi')
+    #tts(msg, PRIO['HIGH'], ttsRoom='Kitchen', ttsVol=42, ttsLang='en-US', ttsVoice='Matthew')
+    #tts(msg, None, ttsRoom='All', ttsLang='de-DE', ttsVoice='Vicki')
+    #tts(msg) # Also works if you accept the defaults
---
+++ Docs/Python/README.md
@@ -0,0 +1,21 @@
+# Jython (Python) Help Libraries for openHAB Scripted Automation
+
+These libraries can be used with [openHAB](http://openhab.org/) (post OH 2.4 snapshot build 1318) and the new rule engine. 
+A previous version that is compatible with earlier versions of OH, but with reduced functionality, has been archived to [this branch](https://github.com/OH-Jython-Scripters/openhab2-jython/tree/original_(%3C%3D2.3)).
+
+- [Getting Started](Getting-Started.md)
+    - [Quick Start Guide](Getting-Started.md#quick-start-guide)
+    - [Applications](Getting-Started.md#applications)
+    - [Jython Scripts and Modules](Getting-Started.md#jython-scripts-and-modules)
+    - [File Locations](Getting-Started.md#file-locations)
+- [Modules](Jython-Modules.md)
+- [Component Scripts](Component-Scripts.md)
+- [Example Scripts](Example-Scripts.md)
+- [Defining Rules](Defining-Rules.md)
+    - [Raw Automation API](Defining-Rules.md#raw-automation-api)
+    - [Using Jython Extensions](Defining-Rules.md#using-jython-extensions)
+        - [Rule and Trigger Decorators](Defining-Rules.md#rule-and-trigger-decorators)
+    - [Event object attributes](Event-Object-Attributes.md)
+- [But how do I...?](But-How-Do-I.md)
+
+These works are based on the original contributions of Steve Bate, for which we are very thankful!  :vulcan_salute: 
