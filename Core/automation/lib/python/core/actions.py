f5daf719d9d358522feec212be5b538399faf4fd
Scott Rushworth <openhab@5iver.com>
2019-05-21 08:39:55 -0400

    Add missing Coore Actions (#116)
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

--- Core/automation/lib/python/core/actions.py
+++ Core/automation/lib/python/core/actions.py
@@ -13,3 +13,21 @@ for s in oh1_actions + oh2_actions:
     name = action_class.simpleName
     setattr(_module, name, action_class)
     __all__.append(name)
+
+try:
+    from org.openhab.core.model.script.actions import Exec
+    from org.openhab.core.model.script.actions import HTTP
+    from org.openhab.core.model.script.actions import LogAction
+    from org.openhab.core.model.script.actions import Ping
+except:
+    from org.eclipse.smarthome.model.script.actions import Exec
+    from org.eclipse.smarthome.model.script.actions import HTTP
+    from org.eclipse.smarthome.model.script.actions import LogAction
+    from org.eclipse.smarthome.model.script.actions import Ping
+
+static_imports = [Exec, HTTP, LogAction, Ping]
+
+for s in static_imports:
+    name = s.simpleName
+    setattr(_module, name, s)
+    __all__.append(name)
