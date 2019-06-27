258a039edf3fe3adf4174f321cb64008105140b1
Scott Rushworth <openhab@5iver.com>
2019-06-27 11:57:34 -0400

    Use self-executing anonymous function in osgi.js (#171)
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

--- Core/automation/lib/javascript/core/osgi.js
+++ Core/automation/lib/javascript/core/osgi.js
@@ -1,58 +1,68 @@
+/*
+This library provides functions for getting, registering, unregistering, and
+finding OSGi services.
+*/
+
 'use strict';
 
-var FrameworkUtil = Java.type("org.osgi.framework.FrameworkUtil");
+(function(context) {
+    'use strict';
 
-var _bundle = FrameworkUtil.getBundle(scriptExtension.class);
-var bundle_context = (typeof _bundle !== "undefined") ? _bundle.getBundleContext() : null;
-var registered_services = [];
+    var FrameworkUtil = Java.type("org.osgi.framework.FrameworkUtil");
 
-function get_service(class_or_name) {
-    if (typeof bundle_context !== "undefined") {
-        var classname = (typeof class_or_name === "object") ? class_or_name.getName() : class_or_name;
-        var ref = bundle_context.getServiceReference(classname);
-        return (typeof ref !== "undefined") ? bundle_context.getService(ref) : null;
+    var _bundle = FrameworkUtil.getBundle(scriptExtension.class);
+    var bundle_context = (typeof _bundle !== "undefined") ? _bundle.getBundleContext() : null;
+    var registered_services = [];
+
+    context.get_service = function(class_or_name) {
+        if (typeof bundle_context !== "undefined") {
+            var classname = (typeof class_or_name === "object") ? class_or_name.getName() : class_or_name;
+            var ref = bundle_context.getServiceReference(classname);
+            return (typeof ref !== "undefined") ? bundle_context.getService(ref) : null;
+        }
     }
-}
 
-function find_services(class_name, filter) {
-    if (typeof bundle_context !== "undefined") {
-        var refs = bundle_context.getAllServiceReferences(class_name, filter);
-        if (typeof refs !== "undefined") {
-            var services = [];
-            for (var i = 0, size = refs.length; i < size ; i++) {
-                services.push(bundle_context.getService(refs[i]));
+    context.find_services = function(class_name, filter) {
+        if (typeof bundle_context !== "undefined") {
+            var refs = bundle_context.getAllServiceReferences(class_name, filter);
+            if (typeof refs !== "undefined") {
+                var services = [];
+                for (var i = 0, size = refs.length; i < size ; i++) {
+                    services.push(bundle_context.getService(refs[i]));
+                }
+                return services;
             }
-            return services;
         }
     }
-}
 
-function register_service(service, interface_names, properties) {
-    if (typeof properties !== "undefined") {
-        var util = Java.type("java.util");
-        p = util.Hashtable();
-        for (var i = 0, size = properties.length; i < size ; i++) {
-            p.put(k, v);
+    context.register_service = function(service, interface_names, properties) {
+        if (typeof properties !== "undefined") {
+            var util = Java.type("java.util");
+            p = util.Hashtable();
+            for (var i = 0, size = properties.length; i < size ; i++) {
+                p.put(k, v);
+            }
+            properties = p;
         }
-        properties = p;
+        else {
+            properties = null;
+        }
+        var reg = bundle_context.registerService(interface_names, service, properties);
+        for (var i = 0, size = interface_names.length; i < size ; i++) {
+            registered_services[name] = (service, reg);
+        }
+        return reg;
     }
-    else {
-        properties = null;
-    }
-    var reg = bundle_context.registerService(interface_names, service, properties);
-    for (var i = 0, size = interface_names.length; i < size ; i++) {
-        registered_services[name] = (service, reg);
-    }
-    return reg;
-}
 
-function unregister_service(service) {
-    var keys = registered_services.keys();
-    for (var i = 0, size = keys.length; i < size ; i++) {
-        var registered_service, reg = registered_services[key];
-        if (service == registered_service) {
-            registered_services.splice(i, keys[i]);
-            reg.unregister();
+    context.unregister_service = function(service) {
+        var keys = registered_services.keys();
+        for (var i = 0, size = keys.length; i < size ; i++) {
+            var registered_service, reg = registered_services[key];
+            if (service == registered_service) {
+                registered_services.splice(i, keys[i]);
+                reg.unregister();
+            }
         }
     }
-}
+
+})(this);
