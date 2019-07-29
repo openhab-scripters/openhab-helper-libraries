25878496cfe0f19f359d39613d81f13e5ee70199
Scott Rushworth <openhab@5iver.com>
2019-07-29 17:21:03 -0400

    Updated comment in hello_world
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

--- Script Examples/Python/hello_world.py
+++ Script Examples/Python/hello_world.py
@@ -5,7 +5,7 @@ This script uses a decorated cron rule that will generate logs every 10s and can
 from core.rules import rule
 from core.triggers import when
 
-@rule("Jython Hello World (cron decorators)", description="This is an example cron triggered rule using decorators", tags=["Test tag", "Hello World"])# [description and tags are optional]
+@rule("Jython Hello World (cron decorators)", description="This is an example cron triggered rule using decorators", tags=["Test tag", "Hello World"])# description and tags are optional
 @when("Time cron 0/10 * * * * ?")
 def hello_world_cron_decorators(event):
     hello_world_cron_decorators.log.info("Hello World!")
