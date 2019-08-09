ccc1e7498c8529933dde31472b3cf1b2ae6856ea
Scott Rushworth <openhab@5iver.com>
2019-08-09 05:30:21 -0400

    Small fix
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

--- Core/automation/lib/javascript/core/osgi.js
+++ Core/automation/lib/javascript/core/osgi.js
@@ -11,19 +11,19 @@ finding OSGi services.
     var FrameworkUtil = Java.type("org.osgi.framework.FrameworkUtil");
 
     var _bundle = FrameworkUtil.getBundle(scriptExtension.class);
-    var bundle_context = (typeof _bundle !== null) ? _bundle.getBundleContext() : null;
+    var bundle_context = (_bundle !== null) ? _bundle.getBundleContext() : null;
     var registered_services = [];
 
     context.get_service = function(class_or_name) {
-        if (typeof bundle_context !== null) {
+        if (bundle_context !== null) {
             var classname = (typeof class_or_name === "object") ? class_or_name.getName() : class_or_name;
             var ref = bundle_context.getServiceReference(classname);
-            return (typeof ref !== null) ? bundle_context.getService(ref) : null;
+            return (ref !== null) ? bundle_context.getService(ref) : null;
         }
     }
 
     context.find_services = function(class_name, filter) {
-        if (typeof bundle_context !== null) {
+        if (bundle_context !== null) {
             var refs = bundle_context.getAllServiceReferences(class_name, filter);
             if (refs !== null) {
                 var services = [];
@@ -36,7 +36,7 @@ finding OSGi services.
     }
 
     context.register_service = function(service, interface_names, properties) {
-        if (typeof properties !== null) {
+        if (properties !== null) {
             var util = Java.type("java.util");
             p = util.Hashtable();
             for (var i = 0, size = properties.length; i < size ; i++) {
