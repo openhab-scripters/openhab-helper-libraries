b4b187107770b2a1683966c643e4041470e4c0e6
Michael Murton <6764025+CrazyIvan359@users.noreply.github.com>
2019-08-11 14:26:37 -0400

    remove configuration.py used for testing
    Signed-off-by: Michael Murton <6764025+CrazyIvan359@users.noreply.github.com>

--- Community/Eos Lighting/automation/lib/python/configuration.py
+++
@@ -1,7 +0,0 @@
-# Eos Lighting System
-eos_master_group = "eos_master_group"
-eos_scene_item_prefix = "eos_"
-eos_scene_item_suffix = "_scene"
-#eos_group_item_prefix = ""
-eos_group_item_suffix = "_eos_group"
-eos_reload_item_name="eos_reinit"
