eafebf1242767a6f6d4b02e35cec18d33dfa1343
Rich Koshak <rlkoshak@gmail.com>
2019-08-30 12:36:02 -0600

    Moving the file to the proper location, created an __init__.py, added license info.
    Signed-off-by: Rich Koshak <rlkoshak@gmail.com>

--- Community/Hysteresis/automation/lib/python/community/hysteresis/hysteresis.py
+++ Community/Hysteresis/automation/lib/python/community/hysteresis/hysteresis.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-The Hysteresis module provides an implementation that will tell you whether a
-value is below, above, or inbetween a target value plus or minus an offset.
-
-Functions
+License
 ========
-    - hysteresis: Returns a value indicating if the value is above, below, or 
-      within the hysteresis gap.
-"""
+Copyright (c) 2019 Richard Koshak
+
+This program and accomanying materials are made available under the terms of 
+the Eclipse Public License 2.0 which is available at 
+http://www.eclipse.org/legal/epl-2.0
+
 from org.eclipse.smarthome.core.library.types import QuantityType, DecimalType, PercentType
+"""
 
 @log_traceback
 def hysteresis(target, value, low=0, high=0):
