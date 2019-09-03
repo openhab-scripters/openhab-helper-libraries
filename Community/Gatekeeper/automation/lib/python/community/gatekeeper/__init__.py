e443d1b8575207ff23bcaee4e4949f88831939a4
Rich Koshak <rlkoshak@gmail.com>
2019-09-03 16:16:00 -0600

    Changed copyright message

--- Community/Gatekeeper/automation/lib/python/community/gatekeeper/__init__.py
+++ Community/Gatekeeper/automation/lib/python/community/gatekeeper/__init__.py
@@ -6,13 +6,10 @@ will implement a pause after calling a command lambda before the nest one can be
 called.
 
 Types
-======
+=====
     - Gatekeeper: Implements the Gatekeeper Design Pattering without blocking.
 
 License
-========
-Copyright (c) 2019 Richard Koshak
-This program and accomanying materials are made available under the terms of 
-the Eclipse Public License 2.0 which is available at 
-http://www.eclipse.org/legal/epl-2.0
+=======
+Copyright (c) 2019 openCopyright (c) 2019 Contributors to the openHAB Scripters project
 """
