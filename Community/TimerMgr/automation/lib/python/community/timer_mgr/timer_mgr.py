1f71ce97edfcd06313fe9678ace8660606ae9658
Rich Koshak <rlkoshak@gmail.com>
2019-09-05 13:18:30 -0600

    Added a cancel_all method

--- Community/TimerMgr/automation/lib/python/community/timer_mgr/timer_mgr.py
+++ Community/TimerMgr/automation/lib/python/community/timer_mgr/timer_mgr.py
@@ -1,17 +1,17 @@
 """
 Author: Rich Koshak
 
-Contains a class used to centralize the management of Timers in 
+Contains a class used to centralize the management of Timers in
 cases where one has one Timer per Item.
 
 Types:
 ======
-    - TimerMgr: Centralizes management of Timers associated with one single 
+    - TimerMgr: Centralizes management of Timers associated with one single
     Item (i.e. one Timer per Item).
 
 License:
 ========
-Copyright (c) 2019 Contributors to the openHAB Scripters project 
+Copyright (c) 2019 Contributors to the openHAB Scripters project
 """
 from core.jsr223.scope import ir
 from core.jsr223.scope import items
@@ -24,10 +24,10 @@ class TimerMgr(object):
     Examples:
         .. code-block::
             flapping_timers = TimerMgr()
-           
-            # In a Rule, check to see if a Timer exists for this Item. If one 
-            # exists, log a warning statement that the Item is flapping. 
-            # Otherwise, set a half second timer and update the Time Item 
+
+            # In a Rule, check to see if a Timer exists for this Item. If one
+            # exists, log a warning statement that the Item is flapping.
+            # Otherwise, set a half second timer and update the Time Item
             # associated with the Item.
             flapping_timers.check(event.itemName,
                                   500,
@@ -37,9 +37,9 @@ class TimerMgr(object):
 
             reminder_timers = TimerMgr()
 
-            
-            # In a Rule, if the door is OPEN, create a timer to go off in 60 
-            # minutes to post a message to the Alert Item. If it's NIGHT time, 
+
+            # In a Rule, if the door is OPEN, create a timer to go off in 60
+            # minutes to post a message to the Alert Item. If it's NIGHT time,
             # reschedule the Timer. If the door is CLOSED, cancel the reminder
             # Timer.
             if items[itemName == OPEN]:
@@ -60,7 +60,7 @@ class TimerMgr(object):
         otherwise it creates a Timer to call the passed in function.
         - has_timer: Returns True if there is an active Timer for the passed in
         Item.
-        - cancel: Cancels the Timer assocaited with the passed in Item, if one 
+        - cancel: Cancels the Timer assocaited with the passed in Item, if one
         exists.
     """
 
@@ -69,7 +69,7 @@ class TimerMgr(object):
         self.timers = {}
 
     def __not_flapping(self, itemName):
-        """Called when the Timer expires. Call the function and delete the timer 
+        """Called when the Timer expires. Call the function and delete the timer
         from the dict. This function ensures that the dict get's cleaned up.
 
         Args:
@@ -78,21 +78,21 @@ class TimerMgr(object):
         try:
             self.timers[itemName]['not_flapping']()
         finally:
-            if itemName in self.timers: 
+            if itemName in self.timers:
                 del self.timers[itemName]
 
-    def check(self, itemName, interval, function, flapping_function=None, 
+    def check(self, itemName, interval, function, flapping_function=None,
                     reschedule=False):
-        """Call to check whether an Item has a Timer. If no Timer exists, create 
+        """Call to check whether an Item has a Timer. If no Timer exists, create
         a new timer to run the passed in function. If a Timer exists, reschedule
         it if reschedule is True and if a flapping_function was passed, run it.
 
         Arguments:
             - itemName: The name of the Item we want to set a Timer on.
-            - interval: How long from now to set the Timer to call function, in 
+            - interval: How long from now to set the Timer to call function, in
             milliseconds.
             - function: Function to call when the Timer expires
-            - flapping_function: Optional function to call if the Item already 
+            - flapping_function: Optional function to call if the Item already
             has a Timer running. Defaults to None.
             - reschedule: Optional flag that causes the Timer to be rescheduled
             when the Item already has a Timer. Defaults to False.
@@ -111,14 +111,14 @@ class TimerMgr(object):
         # No timer exists, create the Timer
         else:
             item = ir.getItem(itemName)
-            timer = ScriptExecution.createTimer(timeout, 
-                        lambda: self.__not_flapping(itemName)) 
+            timer = ScriptExecution.createTimer(timeout,
+                        lambda: self.__not_flapping(itemName))
             self.timers[itemName] = { 'orig_state':   item.state,
                                       'timer':        timer,
                                       'flapping':     flapping_function,
                                       'not_flapping': function }
 
-    def has_timer(self, itemName): 
+    def has_timer(self, itemName):
         """Checks to see if a Timer exists for the passed in Item.
 
         Arguments:
@@ -133,7 +133,15 @@ class TimerMgr(object):
         Arguments:
             - itemName: Name of the Item to cancel the associated Timer.
         """
-        if not self.has_timer(itemName): 
+        if not self.has_timer(itemName):
             return
-        self.timers[itemName]['timer'].cancel() 
+        self.timers[itemName]['timer'].cancel()
         del self.timers[itemName
+
+    def cancel_all(self):
+        """
+        Cancels any existing Timers.
+        """
+        for timer in self.timers.values():
+            if not timer['timer'].hasTerminated():
+                timer['timer'].cancel()
