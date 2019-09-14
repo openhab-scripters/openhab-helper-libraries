15ace2fc929ee29e0c465fc46c59b7aae4760e29
Scott Rushworth <openhab@5iver.com>
2019-09-14 02:35:14 -0400

    [items] Change jsr223.scope import and update docs
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

--- Core/automation/lib/python/core/items.py
+++ Core/automation/lib/python/core/items.py
@@ -1,11 +1,11 @@
 """
 This module allows runtime creation and removal of items. It will also remove
-any links from an Item before it is removed. This module requires the
-JythonItemProvder and JythonItemChannelLinkProvider component scripts.
+any links from an Item before it is removed.
 """
+__all__ = ["add_item", "remove_item"]
 
-from core.jsr223 import scope
-scope.scriptExtension.importPreset(None)
+from core.jsr223.scope import scriptExtension, itemRegistry
+scriptExtension.importPreset(None)
 
 import core
 from core import osgi
@@ -26,17 +26,38 @@ ManagedItemProvider = osgi.get_service(
 
 log = logging.getLogger("{}.core.items".format(LOG_PREFIX))
 
-__all__ = ["add_item", "remove_item"]
-
 def add_item(item_or_item_name, item_type=None, category=None, groups=None, label=None, tags=[], gi_base_type=None, group_function=None):
+    """
+    Adds an Item using a ManagedItemProvider.
+
+    Args:
+        item_or_item_name (Item or str): Item object or name for the Item to
+            create
+        item_type (str): (optional, if item_oritem_name is an Item) the type
+            of the Item
+        category (str): (optional) the category (icon) for the Item
+        groups (str): (optional) a list of groups the Item is a member of
+        label (str): (optional) the label for the Item
+        tags (list): (optional) a list of tags for the Item
+        gi_base_type (str): (optional) the group Item base type for the Item
+        group_function (GroupFunction): (optional) the group function used by
+            the Item
+
+    Returns:
+        Item or None: The Item that was created or None
+
+    Raises:
+        TypeError: if item_or_item_name is not an Item or string, or if
+            item_or_item_name is not an Item and item_type is not provided
+    """
     try:
         if not isinstance(item_or_item_name, basestring) and not hasattr(item_or_item_name, 'name'):
-            raise Exception("\"{}\" is not a string or Item".format(item_or_item_name))
+            raise TypeError("\"{}\" is not a string or Item".format(item_or_item_name))
         item = item_or_item_name
         if isinstance(item_or_item_name, basestring):
             item_name = item_or_item_name
             if item_type is None:
-                raise Exception("Must provide item_type when creating an Item by name")
+                raise TypeError("Must provide item_type when creating an Item by name")
 
             base_item = None if item_type != "Group" or gi_base_type is None else ItemBuilderFactory.newItemBuilder(gi_base_type, item_name + "_baseItem").build()
             group_function = None if item_type != "Group" else group_function
@@ -58,6 +79,16 @@ def add_item(item_or_item_name, item_type=None, category=None, groups=None, labe
         return None
 
 def remove_item(item_or_item_name):
+    """
+    This function removes an Item using a ManagedItemProvider.
+
+    Args:
+        item_or_item_name (Item or str): the Item object or name for the
+            Item to create
+
+    Returns:
+        Item or None: the Item that was removed or None
+    """
     try:
         item = remove_all_links(item_or_item_name)
         if item is None:
@@ -65,7 +96,7 @@ def remove_item(item_or_item_name):
             return None
 
         ManagedItemProvider.remove(item.name)
-        if scope.itemRegistry.getItems(item.name) == []:
+        if itemRegistry.getItems(item.name) == []:
             log.debug("Item removed: [{}]".format(item.name))
             return item
         else:
