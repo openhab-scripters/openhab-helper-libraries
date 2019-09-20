bff9dc05bc7e9b16f7a6894233590f298a36a4a8
Rich Koshak <rlkoshak@gmail.com>
2019-09-20 11:21:52 -0600

    updated online script to use the action's retained flag

--- Community/MQTT Eventbus/automation/jsr223/python/community/mqtt_eventbus/eventbus_online.py
+++ Community/MQTT Eventbus/automation/jsr223/python/community/mqtt_eventbus/eventbus_online.py
@@ -10,7 +10,7 @@ Copyright (c) 2019 contributors to the openHAB Scripters project
 """
 from core.rules import rule
 from core.triggers import when
-from configuration import eb_name, eb_up_br
+from configuration import eb_name, eb_br
 
 @rule("Eventbus Online",
       description="Publsih that this instance of OH is now online",
@@ -20,14 +20,9 @@ def online(event):
     """
     At OH system start, publish "ONLINE" to eb_name/status to indicate that OH
     has come back online. eb_name is defined in configuration and contains the
-    unique name for this OH instance. The Broker Thing ID stored in eb_up_br is
-    used. I recommend setting the retained flag to true for this Thing.
-
-    I also recommend that the LWT configured on the eb_up_br Thing be the same
-    topic used here. This will allow any client to instantly know whether this
-    OH is online whether or not that client happened to be itself online when
-    this OH went offline or came back online.
+    unique name for this OH instance. The Broker Thing ID stored in eb_br is
+    used. I recommend setting the retained flag to true for this Thing's LWT.
     """
     online.log.debug("Reported eventbus as online")
-    actions.get("mqtt", eb_up_br).publishMQTT("{}/status".format(eb_name),
-                                                                 "ONLINE")
+    actions.get("mqtt", eb_br).publishMQTT("{}/status".format(eb_name),
+                                           "ONLINE", True)
