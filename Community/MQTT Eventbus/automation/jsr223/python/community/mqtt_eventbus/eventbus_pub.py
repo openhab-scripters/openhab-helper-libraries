fa8f873a669147f2700acf7ce3fb19b6836cc415
Rich Koshak <rlkoshak@gmail.com>
2019-09-20 11:19:06 -0600

    changed to use retained flag on teh action

--- Community/MQTT Eventbus/automation/jsr223/python/community/mqtt_eventbus/eventbus_pub.py
+++ Community/MQTT Eventbus/automation/jsr223/python/community/mqtt_eventbus/eventbus_pub.py
@@ -10,7 +10,7 @@ Copyright (c) 2019 Contributors to the openHAB Scripters project
 """
 from core.rules import rule
 from core.triggers import when
-from configuration import eb_out_gr, eb_name, eb_cmd_br, eb_up_br
+from configuration import eb_out_gr, eb_name, eb_br
 
 @rule("Publish Item events",
       description=("Publishes commands and update events for all members of {} "
@@ -31,18 +31,14 @@ def pub(event):
     For example, if the Item Foo received a command and eb_name is "appartment"
     the topic would be "appartment/out/Foo/command".
 
-    One can use a different broker Thing for updates and for commands. This
-    should let you, for example, set the retained flag to true for updates and
-    set the retained flag to false for commands.
-        - eb_cmd_br: defined in configuration, the broker Thing ID used for
-        commands. I recommend the retained flag be set to off for this Thing.
-        - eb_up_br: defined in configuration, the broker Thing ID used for
-        updates. I recommend the retained flag be set to on for this Thing.
+    The Broker Thing used to publish the messages is stored in eb_br. Commands
+    are published with the retained flag set to False. Updates are published
+    with the retained flag set to True.
     """
     is_cmd = hasattr(event, 'itemCommand')
     msg = str(event.itemCommand if is_cmd else event.itemState)
     topic = "{}/out/{}/{}".format(eb_name, event.itemName,
                                   "command" if is_cmd else "state")
-    broker = eb_cmd_br if is_cmd else eb_up_br
-    pub.log.debug("Publishing {} to {} on {}".format(msg, topic, broker))
-    actions.get("mqtt", broker).publishMQTT(topic, msg)
+    retained = False if is_cmd else True
+    pub.log.debug("Publishing {} to {} on {}".format(msg, topic, eb_br))
+    actions.get("mqtt", broker).publishMQTT(topic, msg, retained)
