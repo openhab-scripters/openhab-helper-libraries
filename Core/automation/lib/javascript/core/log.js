53cc3876bf8160c98526b30cda00c5b5f73b9611
Martin Stangl <martin.stangl@theblacklion.net>
2019-10-23 22:17:46 +0200

    Change to message format and logger name prefix
    Log message format changed for better readability.
    Private functions actually made private.
    Blank line added after end of code.
    Logger name prefix aligned with LogAction.
    
    Signed-off-by: Martin Stangl <martin.stangl@theblacklion.net>
    
    Signed-off-by: Martin Stangl <martin.stangl@theblacklion.net>

--- Core/automation/lib/javascript/core/log.js
+++ Core/automation/lib/javascript/core/log.js
@@ -1,16 +1,16 @@
 /**
  * Logging functions and variables
- * 
+ *
  * Copyright (c) 2019 Contributors to the openHAB Scripters project
- * 
+ *
  * @author Martin Stangl - initial contribution
  */
-'use strict';	
+'use strict';
 
 (function context (context) {
-    'use strict';	
-    
-    load(__DIR__+'/actions.js');        
+    'use strict';
+
+    load(__DIR__+'/actions.js');
 
     context.NOTIFY_OFF = 0;
     context.NOTIFY_ERROR = 200;
@@ -18,182 +18,191 @@
     context.NOTIFY_INFO = 400;
     context.NOTIFY_DEBUG = 500;
 
-    context.Logger = function Logger (name, notificationLevel, config) {        
-        if (name === undefined) {
-            name = Error().stack.split('\n')[2].split('/').slice(-2).join('.').split(':')[0];
-            name = name.slice(-3) == ".js" ? name.slice(0,-3) : null;
+    context.Logger = function Logger (_name, _notificationLevel, _config) {
+
+        var LOGGER_PREFIX = "org.eclipse.smarthome.model.script.jsr223.javascript";
+
+        var PATH_SEPARATOR = Java.type("java.lang.System").getenv("OPENHAB_CONF").split(Java.type("java.lang.System").getenv("OPENHAB_HOME")).pop()[0];
+        var AUTOMATION_PATH = Java.type("java.lang.System").getenv("OPENHAB_CONF")
+        if (PATH_SEPARATOR === "\\") AUTOMATION_PATH = AUTOMATION_PATH.replace(/\\/g,'/');
+        AUTOMATION_PATH = AUTOMATION_PATH + "/automation/";
+
+        if (_name === undefined) {
+            _name = Error().stack.split('\n')[2].split('/').slice(-2).join('.').split(':')[0];
+            _name = _name.slice(-3) == ".js" ? _name.slice(0,-3) : null;
         }
-        var _logger  = Java.type("org.slf4j.LoggerFactory").getLogger(name === null ? "jsr223.javascript" : "jsr223.javascript." + name.toString().toLowerCase());        
-        var _messageFormatter = Java.type("org.slf4j.helpers.MessageFormatter");
+        var _logger  = Java.type("org.slf4j.LoggerFactory").getLogger(_name === null ? LOGGER_PREFIX : LOGGER_PREFIX + "." + _name.toString().toLowerCase());
 
         try {
             // Set default config for config params not provided.
-            if (config === undefined) config = {};
-            if (config.ERROR === undefined) config.ERROR = {};
-            if (config.WARN  === undefined) config.WARN  = {};
-            if (config.INFO  === undefined) config.INFO  = {};
-            if (config.DEBUG === undefined) config.DEBUG = {};
-            if (config.ERROR.prefix === undefined) config.ERROR.prefix = "short";
-            if (config.WARN.prefix  === undefined) config.WARN.prefix  = "none";
-            if (config.INFO.prefix  === undefined) config.INFO.prefix  = "none";
-            if (config.DEBUG.prefix === undefined) config.DEBUG.prefix = "short";       
+            if (_config === undefined) _config = {};
+            if (_config.ERROR === undefined) _config.ERROR = {};
+            if (_config.WARN  === undefined) _config.WARN  = {};
+            if (_config.INFO  === undefined) _config.INFO  = {};
+            if (_config.DEBUG === undefined) _config.DEBUG = {};
+            if (_config.ERROR.prefix === undefined) _config.ERROR.prefix = "short";
+            if (_config.WARN.prefix  === undefined) _config.WARN.prefix  = "none";
+            if (_config.INFO.prefix  === undefined) _config.INFO.prefix  = "none";
+            if (_config.DEBUG.prefix === undefined) _config.DEBUG.prefix = "short";
+
+            var _MessageFormatter = Java.type("org.slf4j.helpers.MessageFormatter");
+
+            var _getLogMessage = function _getLogMessage (msg, prefix, levelString) {
+                if (msg instanceof Error) {
+                    msg.caller = msg.stack.split('\n\tat ')[1].split(' (')[0]
+                } else {
+                    msg = Error(msg);
+                    msg = _getCallerDetails(msg);
+                }
+                msg = _legacyLoggerCorrection(msg);
+
+                if (prefix === undefined) prefix = "log";
+
+                if (prefix == "none") {
+                    return msg.message;
+                }
+
+                var level = "";
+                var nameText = "";
+                if (prefix != "log") {
+                    level = "[" + levelString + "] ";
+                    nameText = _name === null ? "" : _name;
+                }
+
+                if (prefix == "level") {
+                    return (level + msg.message);
+                }
+
+                var callerText;
+                if (msg.caller.substr(0,1) == "<") {
+                    callerText = ", in " + msg.caller;
+                } else {
+                    callerText = ", function " + msg.caller;
+                }
+
+                var message = msg.message == "" ? "" : msg.message + " ";
+
+                if (prefix == "short") {
+                    return (level + message + "\t\t[" + (nameText != "" ? nameText + ", " : "") + msg.fileName.split('/').pop() + ":" + msg.lineNumber + callerText + "]");
+                }
+                return (level + message + "\t\t[" + (nameText != "" ? nameText + ": " : "") + "at source " + msg.fileName.split(AUTOMATION_PATH).pop() + ", line " + msg.lineNumber + callerText + "]");
+            }
+
+            var _getCallerDetails = function _getCallerDetails (msg) {
+                var matches = msg.stack.split('\n\tat ')[3].match(/(.+?) \((.+):(\d+?)\)/);
+                msg.caller = matches[1];
+                msg.fileName = matches[2]
+                msg.lineNumber = matches[3]
+                return msg;
+            }
+
+            var _sendNotification = function _sendNotification (message, icon, levelString, log) {
+                if (_config[levelString].recipients !== undefined) {
+                    _config[levelString].recipients.forEach(function(mail){
+                        NotificationAction.sendNotification(mail, message, icon, levelString);
+                    })
+                    if (_config[levelString].recipients.length > 0) {
+                        log.trace(Error("Notification sent to " + _config[levelString].recipients.join(", ") + ". Message: \"" + message + "\""));
+                    }
+                } else {
+                    NotificationAction.sendBroadcastNotification(message, icon, levelString);
+                    log.trace(Error("Broadcast notification sent. Message: \"" + message + "\""));
+                }
+            }
+
+            var _legacyLoggerCorrection = function _legacyLoggerCorrection (msg) {
+                if (msg.fileName.search(/automation\/lib\/javascript\/core\/utils\.js$/) !== -1) {
+                    switch (msg.caller) {
+                        case "logError":
+                        case "logWarn":
+                        case "logInfo":
+                        case "logDebug":
+                        case "logTrace":
+                        case "error":
+                        case "warn":
+                        case "info":
+                        case "debug":
+                        case "log":
+                            var stackArray = msg.stack.split('\n\tat ');
+                            stackArray.splice(3,1);
+                            msg.stack = stackArray.join('\n\tat ');
+                            msg = _getCallerDetails(msg);
+                        }
+                }
+                return msg;
+            }
 
             return Object.create(Object.prototype, {
-                _notificationLevel: { value: (notificationLevel === undefined || notificationLevel === null) ? context.NOTIFY_OFF : notificationLevel },
-                _config: { value: config },                
-                _name: { value: name },
+                notificationLevel: { value: (_notificationLevel === undefined || _notificationLevel === null) ? context.NOTIFY_OFF : _notificationLevel },
+                config: { value: _config },
+                name: { value: _name },
 
                 error: { value: function error (msg) {
                     try {
-                        if (_logger.isErrorEnabled()) {                                                  
-                            _logger.error(this._getLogMessage(msg), [].slice.call(arguments).slice(1));
+                        if (_logger.isErrorEnabled()) {
+                            _logger.error(_getLogMessage(msg), [].slice.call(arguments).slice(1));
                         }
-                        if (this._notificationLevel >= context.NOTIFY_ERROR) {
-                            this._sendNotification(_messageFormatter.arrayFormat(this._getLogMessage(msg, this._config.ERROR.prefix, "ERROR"), [].slice.call(arguments).slice(1)).getMessage(), "fire", "ERROR");
+                        if (this.notificationLevel >= context.NOTIFY_ERROR) {
+                            _sendNotification(_MessageFormatter.arrayFormat(_getLogMessage(msg, this.config.ERROR.prefix, "ERROR"), [].slice.call(arguments).slice(1)).getMessage(), "fire", "ERROR", this);
                         }
                     } catch (err) {
-                        _logger.error(this._getLogMessage(err));
+                        _logger.error(_getLogMessage(err));
                     }
-                }},              
+                }},
 
                 warn: { value: function warn (msg) {
                     try {
-                        if (_logger.isWarnEnabled()) {                      
-                            _logger.warn(this._getLogMessage(msg), [].slice.call(arguments).slice(1));
+                        if (_logger.isWarnEnabled()) {
+                            _logger.warn(_getLogMessage(msg), [].slice.call(arguments).slice(1));
                         }
-                        if (this._notificationLevel >= context.NOTIFY_WARN) {
-                            this._sendNotification(_messageFormatter.arrayFormat(this._getLogMessage(msg, this._config.WARN.prefix, "WARN"), [].slice.call(arguments).slice(1)).getMessage(), "error", "WARN");
+                        if (this.notificationLevel >= context.NOTIFY_WARN) {
+                            _sendNotification(_MessageFormatter.arrayFormat(_getLogMessage(msg, this.config.WARN.prefix, "WARN"), [].slice.call(arguments).slice(1)).getMessage(), "error", "WARN", this);
                         }
                     } catch (err) {
-                        _logger.error(this._getLogMessage(err));
+                        _logger.error(_getLogMessage(err));
                     }
                 }},
 
                 info: { value: function info (msg) {
                     try {
-                        if (_logger.isInfoEnabled()) {                      
-                            _logger.info(this._getLogMessage(msg), [].slice.call(arguments).slice(1));
+                        if (_logger.isInfoEnabled()) {
+                            _logger.info(_getLogMessage(msg), [].slice.call(arguments).slice(1));
                         }
-                        if (this._notificationLevel >= context.NOTIFY_INFO) {
-                            this._sendNotification(_messageFormatter.arrayFormat(this._getLogMessage(msg, this._config.INFO.prefix, "INFO"), [].slice.call(arguments).slice(1)).getMessage(), "lightbulb", "INFO");
+                        if (this.notificationLevel >= context.NOTIFY_INFO) {
+                            _sendNotification(_MessageFormatter.arrayFormat(_getLogMessage(msg, this.config.INFO.prefix, "INFO"), [].slice.call(arguments).slice(1)).getMessage(), "lightbulb", "INFO", this);
                         }
                     } catch (err) {
-                        _logger.error(this._getLogMessage(err));
+                        _logger.error(_getLogMessage(err));
                     }
                 }},
 
                 debug: { value: function debug (msg) {
                     try {
-                        if (_logger.isDebugEnabled()) {                      
-                            _logger.debug(this._getLogMessage(msg), [].slice.call(arguments).slice(1));
+                        if (_logger.isDebugEnabled()) {
+                            _logger.debug(_getLogMessage(msg), [].slice.call(arguments).slice(1));
                         }
-                        if (this._notificationLevel >= context.NOTIFY_DEBUG) {
-                            this._sendNotification(_messageFormatter.arrayFormat(this._getLogMessage(msg, this._config.DEBUG.prefix, "DEBUG"), [].slice.call(arguments).slice(1)).getMessage(), "text", "DEBUG");
+                        if (this.notificationLevel >= context.NOTIFY_DEBUG) {
+                            _sendNotification(_MessageFormatter.arrayFormat(_getLogMessage(msg, this.config.DEBUG.prefix, "DEBUG"), [].slice.call(arguments).slice(1)).getMessage(), "text", "DEBUG", this);
                         }
                     } catch (err) {
-                        _logger.error(this._getLogMessage(err));
+                        _logger.error(_getLogMessage(err));
                     }
                 }},
 
                 trace: { value: function trace (msg) {
-                    try {                        
-                        if (_logger.isTraceEnabled()) {     
-                            _logger.trace(this._getLogMessage(msg), [].slice.call(arguments).slice(1));                 
+                    try {
+                        if (_logger.isTraceEnabled()) {
+                            _logger.trace(_getLogMessage(msg), [].slice.call(arguments).slice(1));
                         }
-                    } catch (err) {                        
-                        _logger.error(this._getLogMessage(err));                        
+                    } catch (err) {
+                        _logger.error(_getLogMessage(err));
                     }
-                }},
-                
-                _getCallerDetails: { value: function _getCallerDetails (msg) {
-                    var matches = msg.stack.split('\n\tat ')[3].match(/(.+?) \((.+):(\d+?)\)/);
-                    msg.caller = matches[1];
-                    msg.fileName = matches[2]
-                    msg.lineNumber = matches[3]   
-                    return msg;
-                }},       
-                
-                _getLogMessage: { value: function _getLogMessage (msg, prefix, levelString) {                                        
-                    if (msg instanceof Error) {
-                        msg.caller = msg.stack.split('\n\tat ')[1].split(' (')[0]
-                    } else {
-                        msg = Error(msg);
-                        msg = this._getCallerDetails(msg);                    
-                    }
-                    msg = this._legacyLoggerCorrection(msg);
-
-                    if (prefix === undefined) prefix = "log";                                                            
-                    
-                    if (prefix == "none") {
-                        return msg.message;
-                    }
-
-                    var level = "";
-                    var name = "";
-                    if (prefix != "log") {
-                        level = "[" + levelString + "] ";
-                        name = this._name === null ? "" : this._name;
-                    }
-
-                    if (prefix == "level") {
-                        return (level + msg);
-                    }
-
-                    var callerText;
-                    if (msg.caller.substr(0,1) == "<") {
-                        callerText = ", in " + msg.caller;
-                    } else {
-                        callerText = ", function " + msg.caller;
-                    }
-                    
-                    var message = msg.message == "" ? "" : "] " + msg.message;                    
-
-                    if (prefix == "short") {
-                        return (level + "[" + (name != "" ? name + ", " : "") + msg.fileName.split('/').pop() + ":" + msg.lineNumber + callerText + message);    
-                    }
-                    return (level + "[" + (name != "" ? name + ": " : "") + "source " + msg.fileName + ", line " + msg.lineNumber + callerText + message);
-                }},
-                
-                _legacyLoggerCorrection: { value: function _legacyLoggerCorrection (msg) {
-                    if (msg.fileName.search(/automation\/lib\/javascript\/core\/utils\.js$/) !== -1) {
-                        switch (msg.caller) {
-                            case "logError":
-                            case "logWarn":
-                            case "logInfo":
-                            case "logDebug":
-                            case "logTrace":
-                            case "error":
-                            case "warn":
-                            case "info":
-                            case "debug":
-                            case "log":
-                                var stackArray = msg.stack.split('\n\tat ');
-                                stackArray.splice(3,1);
-                                msg.stack = stackArray.join('\n\tat ');
-                                msg = this._getCallerDetails(msg);
-                            }
-                    }
-                    return msg;
-                }},
-
-                _sendNotification: { value: function _sendNotification (message, icon, levelString) {                                                                                
-                    if (this._config[levelString].recipients !== undefined) {
-                        this._config[levelString].recipients.forEach(function(mail){             
-                            NotificationAction.sendNotification(mail, message, icon, levelString);                            
-                        })
-                        if (this._config[levelString].recipients.length > 0) {
-                            this.trace(Error("Notification sent to " + this._config[levelString].recipients.join(", ") + ". Message: \"" + message + "\""));
-                        }                         
-                    } else {                        
-                        NotificationAction.sendBroadcastNotification(message, icon, levelString);
-                        this.trace(Error("Broadcast notification sent. Message: \"" + message + "\""));
-                    }                    
                 }}
 
             })
         } catch (err) {
-            _logger.error(err.fileName + ", line " + err.lineNumber + ": " + err.message);
+            _logger.error(err.message + " [source " + err.fileName + ", line " + err.lineNumber + "]");
         }
-    }    
+    }
 
 })(this);
