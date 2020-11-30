23cf02f976e6ed59950f2aa457be97d05bc09537
Scott Rushworth <github@5iver.com>
2020-11-30 03:38:28 -0500

    Renamed 000_startup.py (#365)
    Signed-off-by: Scott Rushworth <github@5iver.com>

--- Core/automation/jsr223/python/core/000_Startup.py
+++ Core/automation/jsr223/python/core/000_Startup.py
@@ -1,23 +1,31 @@
+# pylint: disable=unused-import
 """
 This is the first script to load and it will log system information to assist
 with troubleshooting.
 """
 from org.slf4j import LoggerFactory
-LOG = LoggerFactory.getLogger("jython.Startup")
+LOG = LoggerFactory.getLogger("jython.startup")
 
 from java.lang import System
 import sys
 
+try:
+    import configuration
+    configuration_py_installed = True
+except:
+    configuration_py_installed = False
+
 LOG.warn("\n\n\
 *******************************************************************************\n\
-Jython version:       {}.{}.{}.{}\n\
-Operating system:     {}\n\
-OS Version:           {}\n\
-Java vendor:          {}\n\
-Java VM name:         {}\n\
-Java runtime name:    {}\n\
-Java runtime version: {}\n\
-sys.path:             {}\n\
+Jython version:             {}.{}.{}.{}\n\
+Operating system:           {}\n\
+OS Version:                 {}\n\
+Java vendor:                {}\n\
+Java VM name:               {}\n\
+Java runtime name:          {}\n\
+Java runtime version:       {}\n\
+configuration.py installed: {}\n\
+sys.path:                   {}\n\
 *******************************************************************************\n".format(
 sys.version_info[0], sys.version_info[1], sys.version_info[2], sys.version_info[3],
 System.getProperty("os.name"),
@@ -26,4 +34,5 @@ System.getProperty("java.vendor"),
 System.getProperty("java.vm.name"),
 System.getProperty("java.runtime.name"),
 System.getProperty("java.runtime.version"),
-"\n                      ".join(sys.path)))
+str(configuration_py_installed),
+"\n                            ".join(sys.path)))
