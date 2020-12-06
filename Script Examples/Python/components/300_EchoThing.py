92650bc237d3e2b96f98520a6088b932e1282345
Scott Rushworth <github@5iver.com>
2020-12-06 01:59:22 -0500

    Move unused/nonworking scripts to Examples (#367)
    Signed-off-by: Scott Rushworth <github@5iver.com>

--- Script Examples/Python/components/300_EchoThing.py
+++ Script Examples/Python/components/300_EchoThing.py
@@ -11,22 +11,24 @@ Requires several other components:
 
 Note: there are load ordering issues that will need to be resolved.
 """
-
 scriptExtension.importPreset(None)
+
+thing_handler_factory = None
 
 try:
     from org.openhab.core.thing import ThingTypeUID, ChannelUID
-    from org.openhab.core.thing.type import ThingType, ChannelTypeUID, ChannelType, ChannelDefinition
-    from org.openhab.core.thing.binding import ThingFactory, ThingHandlerFactory, BaseThingHandlerFactory, BaseThingHandler, ThingTypeProvider
+    from org.openhab.core.thing.type import ThingType, ChannelTypeUID, ChannelDefinition#, ChannelType
+    from org.openhab.core.thing.binding import ThingFactory, ThingHandlerFactory, BaseThingHandler
+    thing_handler_factory = "org.openhab.core.thing.binding.ThingHandlerFactory"
 except:
     from org.eclipse.smarthome.core.thing import ThingTypeUID, ChannelUID
-    from org.eclipse.smarthome.core.thing.type import ThingType, ChannelTypeUID, ChannelType, ChannelDefinition
-    from org.eclipse.smarthome.core.thing.binding import ThingFactory, ThingHandlerFactory, BaseThingHandlerFactory, BaseThingHandler, ThingTypeProvider
-
+    from org.eclipse.smarthome.core.thing.type import ThingType, ChannelTypeUID, ChannelDefinition#, ChannelType
+    from org.eclipse.smarthome.core.thing.binding import ThingFactory, ThingHandlerFactory, BaseThingHandler
+    thing_handler_factory = "org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory"
 try:
-    from org.openhab.core.binding import BindingInfo, BindingInfoProvider
+    from org.openhab.core.binding import BindingInfo
 except:
-    from org.eclipse.smarthome.core.binding import BindingInfo, BindingInfoProvider
+    from org.eclipse.smarthome.core.binding import BindingInfo
 
 import core
 from core.osgi import register_service, unregister_service, get_service
@@ -34,9 +36,9 @@ from core.osgi import register_service, unregister_service, get_service
 BINDING_ID = "jython"
 INPUT_CHANNEL_NAME = "input"
 OUTPUT_CHANNEL_NAME = "output"
-
 THING_NAME = "echo"
 THING_TYPE_UID = ThingTypeUID(BINDING_ID, THING_NAME)
+LOG = core.log.logging.getLogger("{}.{}".format(BINDING_ID, THING_NAME))
 
 config_description_registry = get_service(
         "org.openhab.core.config.core.ConfigDescriptionRegistry"
@@ -49,59 +51,62 @@ thing_type_registry = get_service(
         "org.eclipse.smarthome.core.thing.type.ThingTypeRegistry"
     )
 
-log = core.log.logging.getLogger("{}.{}".format(BINDING_ID, THING_NAME))
 
 class EchoThingHandler(BaseThingHandler):
+
     def __init__(self, thing):
         BaseThingHandler.__init__(self, thing)
         self.input_channel_id = ChannelUID(thing.getUID(), INPUT_CHANNEL_NAME)
         self.output_channel_id = ChannelUID(thing.getUID(), OUTPUT_CHANNEL_NAME)
-        log.debug('output_channel_id: %s', self.output_channel_id)
+        LOG.debug('output_channel_id: %s', self.output_channel_id)
 
     def handleCommand(self, channelUID, command):
-        log.debug('handleCommand: %s %s', channelUID, command)
+        LOG.debug('handleCommand: %s %s', channelUID, command)
 
     def handleUpdate(self, channelUID, newState):
-        log.debug('handleUpdate %s %s', channelUID, newState)
+        LOG.debug('handleUpdate %s %s', channelUID, newState)
         self.updateState(self.output_channel_id, newState)
 
+
 class EchoThingHandlerFactory(ThingHandlerFactory):
+
     def supportsThingType(self, thingTypeUID):
-        log.debug('supportsThingType: %s', thingTypeUID)
+        LOG.debug('supportsThingType: %s', thingTypeUID)
         return thingTypeUID == THING_TYPE_UID
 
     def createHandler(self, thing):
         try:
-            log.debug('createHandler: %s', thing)
+            LOG.debug('createHandler: %s', thing)
             if thing.getThingTypeUID() == THING_TYPE_UID:
                 return EchoThingHandler(thing)
         except:
             import traceback
-            print(traceback.format_exc())
+            LOG.debug(traceback.format_exc())
 
     def createThing(self, thingTypeUID, configuration, thingUID, bridgeUID):
         try:
-            log.debug('createThing: %s %s %s %s', thingTypeUID, configuration, thingUID, bridgeUID)
+            LOG.debug('createThing: %s %s %s %s', thingTypeUID, configuration, thingUID, bridgeUID)
             thing_type = thing_type_registry.getThingType(thingTypeUID)
-            log.debug('thing_type: %s', thing_type)
+            LOG.debug('thing_type: %s', thing_type)
             thing = ThingFactory.createThing(
                 thing_type, thingUID, configuration,
                 bridgeUID, config_description_registry)
-            log.debug('thing: %s', thing)
+            LOG.debug('thing: %s', thing)
             return thing
         except:
             import traceback
-            print(traceback.format_exc())
+            LOG.debug(traceback.format_exc())
 
     def registerHandler(self, thing):
-        log.debug('registerHandler: %s', thing)
+        LOG.debug('registerHandler: %s', thing)
         return self.createHandler(thing)
 
     def unregisterHandler(self, thing):
-        log.debug('unregisterHandler: %s', thing)
+        LOG.debug('unregisterHandler: %s', thing)
 
     def removeThing(self, thingUID):
-        log.debug('removeThing: %s', thingUID)
+        LOG.debug('removeThing: %s', thingUID)
+
 
 # NOTE: Required mod to Bundle veto component
 
@@ -140,8 +145,10 @@ core.JythonBindingInfoProvider.add(
 
 service = EchoThingHandlerFactory()
 
+
 def scriptLoaded(id):
-    register_service(service, ["org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory"])
+    register_service(service, [thing_handler_factory])
+
 
 def scriptUnloaded():
     unregister_service(service)
--- Script Examples/Python/components/300_JythonConsoleCommand.py
+++ Script Examples/Python/components/300_JythonConsoleCommand.py
@@ -1,39 +1,49 @@
 """
-This script defines a Command extension to the OSGI console. The example
+This script defines a Command extension to the OSGi console. The example
 Command prints some Jython platform details to the console output.
 """
-
 import platform
 
+try:
+    from org.openhab.core.io.console.extensions import AbstractConsoleCommandExtension
+except:
+    from org.eclipse.smarthome.io.console.extensions import AbstractConsoleCommandExtension
+
 from core.osgi import register_service, unregister_service
-from org.eclipse.smarthome.io.console.extensions import AbstractConsoleCommandExtension
 from core.log import logging, LOG_PREFIX
 
-log = logging.getLogger("{}.jythonConsoleCommand".format(LOG_PREFIX))
+LOG = logging.getLogger("{}.JythonConsoleCommand".format(LOG_PREFIX))
+
+service = None
+
 
 class JythonCommand(AbstractConsoleCommandExtension):
     def __init__(self):
-        AbstractConsoleCommandExtension.__init__(self, "jython", "Example Jython command extension")
+        AbstractConsoleCommandExtension.__init__(self, "jython", "Jython command extension")
 
     def getUsages(self):
         return ["jython - (test) Jython interpreter information"]
 
     def execute(self, args, console):
-        print('architecture:', platform.architecture()[0])
-        print('java_ver:', platform.java_ver())
-        print('node:', platform.node())
-        print('processor:', platform.processor())
-        print('python_compiler:', platform.python_compiler())
-        print('python_implementation:', platform.python_implementation())
-        print('python_version:', platform.python_version())
-        print('release:', platform.release())
+        LOG.info('architecture:', platform.architecture()[0])
+        LOG.info('java_ver:', platform.java_ver())
+        LOG.info('node:', platform.node())
+        LOG.info('processor:', platform.processor())
+        LOG.info('python_compiler:', platform.python_compiler())
+        LOG.info('python_implementation:', platform.python_implementation())
+        LOG.info('python_version:', platform.python_version())
+        LOG.info('release:', platform.release())
+
 
 def scriptLoaded(id):
     global service
     service = JythonCommand()
-    interfaces = ["org.eclipse.smarthome.io.console.extensions.ConsoleCommandExtension"]
-    registration = register_service(service, interfaces)
-    log.info("Registered command extension")
+    try:
+        register_service(service, ["org.openhab.core.io.console.extensions.ConsoleCommandExtension"])
+    except:
+        register_service(service, ["org.eclipse.smarthome.io.console.extensions.ConsoleCommandExtension"])
+    LOG.info("Registered command extension")
+
 
 def scriptUnloaded():
     global service
