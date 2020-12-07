2d25612606b551b0bffdd0d1224cec8cf5835312
Scott Rushworth <github@5iver.com>
2020-12-07 01:43:33 -0500

    Fix log_traceback and make configuration optional (#372)
    Signed-off-by: Scott Rushworth <github@5iver.com>

--- Core/automation/lib/python/core/log.py
+++ Core/automation/lib/python/core/log.py
@@ -4,13 +4,25 @@ with the slf4j library used by openHAB. The ``configuration`` module provides
 a ``LOG_PREFIX`` variable that is used as the default logger throughout the
 core modules and scripts.
 """
+__all__ = [
+    "LOG_PREFIX",
+    "logging",
+    "log_traceback"
+]
+
 import logging
-import functools
 import traceback
+from functools import wraps
 
 from org.slf4j import Logger, LoggerFactory
 
-from configuration import LOG_PREFIX
+try:
+    import configuration
+    LOG_PREFIX = configuration.LOG_PREFIX
+except:
+    LOG_PREFIX = "jython"
+    LoggerFactory.getLogger("{}.core.log".format(LOG_PREFIX)).warn("The 'configuration.py' file is missing from teh python.path!")
+
 
 class Slf4jHandler(logging.Handler):
     def emit(self, record):
@@ -31,11 +43,13 @@ class Slf4jHandler(logging.Handler):
         elif level == logging.INFO:
             logger.info(message)
 
-handler = Slf4jHandler()
-logging.root.setLevel(logging.DEBUG)
-logging.root.handlers = [handler]
 
-def log_traceback(fn):
+HANDLER = Slf4jHandler()
+logging.root.setLevel(logging.DEBUG)
+logging.root.handlers = [HANDLER]
+
+
+def log_traceback(function):
     """
     Decorator to provide better Jython stack traces
 
@@ -45,25 +59,26 @@ def log_traceback(fn):
     configuration.adminEmail variable is populated, the notification will be
     sent to that address. Otherwise, a broadcast notification will be sent.
     """
-    functools.wraps(fn)
+    @wraps(function)
     def wrapper(*args, **kwargs):
         try:
-            return fn(*args, **kwargs)
+            return function(*args, **kwargs)
         except:
             rule_name = None
-            if hasattr(fn, 'log'):
-                fn.log.error(traceback.format_exc())
-                rule_name = fn.name
-            elif len(args) > 0 and hasattr(args[0], 'log'):
-                args[0].log.error(traceback.format_exc())
+            if hasattr(function, 'log'):
+                function.log.warn(traceback.format_exc())
+                rule_name = function.name
+            elif args and hasattr(args[0], 'log'):
+                args[0].log.warn(traceback.format_exc())
                 rule_name = args[0].name
             else:
-                logging.getLogger(LOG_PREFIX).error(traceback.format_exc())
+                logging.getLogger(LOG_PREFIX).warn(traceback.format_exc())
             import core.actions
             if hasattr(core.actions, 'NotificationAction'):
-                import configuration
-                if hasattr(configuration, 'admin_email') and configuration.admin_email != "admin_email@some_domain.com":
-                    core.actions.NotificationAction.sendNotification(configuration.admin_email, "Exception: {}: [{}]".format(rule_name, traceback.format_exc()))
-                else:
+                try:
+                    import configuration
+                    if hasattr(configuration, 'admin_email') and configuration.admin_email != "admin_email@some_domain.com":
+                        core.actions.NotificationAction.sendNotification(configuration.admin_email, "Exception: {}: [{}]".format(rule_name, traceback.format_exc()))
+                except:
                     core.actions.NotificationAction.sendBroadcastNotification("Exception: {}: [{}]".format(rule_name, traceback.format_exc()))
     return wrapper
