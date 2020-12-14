03038de498f70e6fa46d2a1457c38c8a4902c4e5
Scott Rushworth <github@5iver.com>
2020-12-14 03:52:09 -0500

    Made things more configurable (#375)
    Signed-off-by: Scott Rushworth <github@5iver.com>

--- Community/Area Triggers and Actions/automation/jsr223/python/community/area_triggers_and_actions/area_triggers.py
+++ Community/Area Triggers and Actions/automation/jsr223/python/community/area_triggers_and_actions/area_triggers.py
@@ -19,6 +19,9 @@ from core.triggers import when
 from core.metadata import get_metadata, get_key_value
 from core.actions import PersistenceExtensions
 
+DEFAULT_ACTION_FUNCTION = AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get("default_action_function") or "light_action"
+MODE_ITEM = AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get("mode_item") or "Mode"
+
 
 @rule("Area triggers", description="This rule will update the light level when the state of an area changes.")
 @when("Member of gArea_Trigger changed")
@@ -34,7 +37,7 @@ def area_triggers(event):
     will also be executed using the trigger group.
     """
     #start_time = DateTime.now().getMillis()
-    if items["Mode"] != StringType("Security"):
+    if items[MODE_ITEM] != StringType("Security"):
         area_triggers.log.debug(u"{}: '{}'".format(event.itemName, event.itemState))
         active = event.itemState in [ON, OPEN]
         action_groups = itemRegistry.getItems(event.itemName.replace("_Trigger", "_Action"))
@@ -43,7 +46,7 @@ def area_triggers(event):
                 #area_triggers.log.warn(u"Test: start loop: itemName: '{}', itemState: '{}', time: '{}'".format(event.itemName, event.itemState, DateTime.now().getMillis() - start_time))
                 for item in action_group.members:
                     item_metadata = get_metadata(item.name, "area_triggers_and_actions")
-                    action_function_names = item_metadata.configuration.keys() if item_metadata else [AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get("default_action_function")]
+                    action_function_names = item_metadata.configuration.keys() if item_metadata else [DEFAULT_ACTION_FUNCTION]
                     for action_function_name in action_function_names:
                         start_new_thread(start_action, (item, active, action_function_name))
                 #area_triggers.log.warn(u"Test: end loop: itemName: '{}', itemState: '{}', time: '{}'".format(event.itemName, event.itemState, DateTime.now().getMillis() - start_time))
@@ -56,10 +59,10 @@ def area_triggers(event):
     #area_triggers.log.warn(u"Test: {}: '{}': time='{}'".format(event.itemName, event.itemState, DateTime.now().getMillis() - start_time))
 
 
-# The following rule will update the lights using the 'light_action'
+# The following rule will update the lights using the default_action_function
 def lux_trigger_generator():
     def generated_triggers(function):
-        lux_item_names = list(set([get_key_value(item.name, "area_triggers_and_actions", "light_action", "lux_item_name") for item in itemRegistry.getAll() if get_key_value(item.name, "area_triggers_and_actions", "light_action", "lux_item_name")]))
+        lux_item_names = list(set([get_key_value(item.name, "area_triggers_and_actions", DEFAULT_ACTION_FUNCTION, "lux_item_name") for item in itemRegistry.getAll() if get_key_value(item.name, "area_triggers_and_actions", "light_action", "lux_item_name")]))
         for lux_item_name in lux_item_names:
             when("Item {} changed".format(lux_item_name))(function)
         return function
@@ -68,7 +71,7 @@ def lux_trigger_generator():
 
 @rule("Mode or lux change", description="This rule will update the light levels when the Mode or lux changes. This is used with the default light_action function.")
 @lux_trigger_generator()
-@when("Item Mode changed")
+@when("Item {} changed".format(AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get("mode_item") or "light_action"))
 @when("System started")
 def mode_or_lux_change(event):
     """
@@ -76,7 +79,7 @@ def mode_or_lux_change(event):
     for groups that are currently active, using the current mode and lux level.
     """
     #start_time = DateTime.now().getMillis()
-    mode_change_or_system_start = (event.itemName == "Mode") if event is not None else True
+    mode_change_or_system_start = (event.itemName == MODE_ITEM) if event is not None else True
     # get a list of active trigger groups (or all of them, if system started)
     for trigger_group in [group for group in itemRegistry.getItem("gArea_Trigger").members]:
         trigger_type = "active" if trigger_group.state in [ON, OPEN] else "inactive"
@@ -85,27 +88,27 @@ def mode_or_lux_change(event):
             for action_group in action_groups:# there should be only 1 or None
                 for item in action_group.members:
                     item_metadata = get_metadata(item.name, "area_triggers_and_actions")
-                    action_function_names = item_metadata.configuration.keys() if item_metadata else [AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get("default_action_function")]
-                    if "light_action" in action_function_names:
-                        light_action_metadata = get_key_value(item.name, "area_triggers_and_actions", "light_action")
-                        lux_item_name = light_action_metadata.get("lux_item_name", AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get("light_action", {}).get("lux_item_name"))
+                    action_function_names = item_metadata.configuration.keys() if item_metadata else [DEFAULT_ACTION_FUNCTION]
+                    if DEFAULT_ACTION_FUNCTION in action_function_names:
+                        light_action_metadata = get_key_value(item.name, "area_triggers_and_actions", DEFAULT_ACTION_FUNCTION)
+                        lux_item_name = light_action_metadata.get("lux_item_name", AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get(DEFAULT_ACTION_FUNCTION, {}).get("lux_item_name"))
                         if mode_change_or_system_start:
-                            start_new_thread(start_action, (item, trigger_group.state in [ON, OPEN], "light_action"))
-                            mode_or_lux_change.log.debug(u"Mode change or System start light adjustment: {}: {}: {}".format(items["Mode"], action_group.name, item.name))
+                            start_new_thread(start_action, (item, trigger_group.state in [ON, OPEN], DEFAULT_ACTION_FUNCTION))
+                            mode_or_lux_change.log.debug(u"Mode change or System start light adjustment: {}: {}: {}".format(items[MODE_ITEM], action_group.name, item.name))
                         else:
                             if lux_item_name == event.itemName and not isinstance(event.itemState, UnDefType):
                                 # the lux Item associated with the action Item has changed state
                                 lux_current = event.itemState.intValue()
                                 lux_previous = event.oldItemState.intValue()
-                            elif lux_item_name is not None and lux_item_name == AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get("light_action", {}).get("lux_item_name") and not isinstance(items[lux_item_name], UnDefType):
+                            elif lux_item_name is not None and lux_item_name == AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION.get(DEFAULT_ACTION_FUNCTION, {}).get("lux_item_name") and not isinstance(items[lux_item_name], UnDefType):
                                 # this action Item is associated to the default lux Item
                                 lux_current = items[lux_item_name].intValue()
                                 lux_previous = PersistenceExtensions.previousState(itemRegistry.getItem(lux_item_name), True).state.intValue()
                             else:
                                 # this action Item is not associated to a lux Item and there is no default lux_item_name configured in AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION
                                 break
-                            lux_trigger = light_action_metadata.get(trigger_type, {}).get("modes", {}).get(items["Mode"].toString(), {}).get("lux_trigger", AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION["light_action"]["default_levels"][trigger_type]["lux_trigger"])
+                            lux_trigger = light_action_metadata.get(trigger_type, {}).get("modes", {}).get(items[MODE_ITEM].toString(), {}).get("lux_trigger", AREA_TRIGGERS_AND_ACTIONS_CONFIGURATION[DEFAULT_ACTION_FUNCTION]["default_levels"][trigger_type]["lux_trigger"])
                             if min(lux_previous, lux_current) < lux_trigger < max(lux_previous, lux_current):
-                                start_new_thread(start_action, (item, trigger_group.state in [ON, OPEN], "light_action"))
+                                start_new_thread(start_action, (item, trigger_group.state in [ON, OPEN], DEFAULT_ACTION_FUNCTION))
                                 mode_or_lux_change.log.debug(u"Lux change light adjustment: {}: {}: current: '{}', previous: '{}'".format(action_group.name, item.name, lux_current, lux_previous))
     #mode_or_lux_change.log.warn(u"Test: time: '{}'".format(DateTime.now().getMillis() - start_time))
